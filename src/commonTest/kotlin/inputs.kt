3ac167f1681c65a8b5d2cc1837e95d7e3c0a57a3
Martynas Petuška <martynas@petuska.dev>
2021-12-04 16:10:46 +0000

    Day4

--- src/commonTest/kotlin/inputs.kt
+++ src/commonTest/kotlin/inputs.kt
@@ -14,19 +14,19 @@ private fun <O> dayInput(day: Int, outputs: List<Output<O>>): DataProvider<O> {
 private const val UNSOLVED = -1
 
 val day1Input: DataProvider<Int> =
-    dayInput(1, listOf(Output(partOne = 7, partTwo = 5), Output(partOne = 1722, partTwo = 1748)))
+  dayInput(1, listOf(Output(partOne = 7, partTwo = 5), Output(partOne = 1722, partTwo = 1748)))
 val day2Input: DataProvider<Int> =
-    dayInput(
-        2,
-        listOf(
-            Output(partOne = 150, partTwo = 2120749), Output(partOne = 900, partTwo = 2138382217)))
+  dayInput(
+    2,
+    listOf(
+      Output(partOne = 150, partTwo = 900), Output(partOne = 2120749, partTwo = 2138382217)))
 val day3Input: DataProvider<Int> =
-    dayInput(
-        3,
-        listOf(Output(partOne = 198, partTwo = 3959450), Output(partOne = 230, partTwo = 7440311)))
+  dayInput(
+    3,
+    listOf(Output(partOne = 198, partTwo = 230), Output(partOne = 3959450, partTwo = 7440311)))
 val day4Input: DataProvider<Int> =
-    dayInput(
-        4,
-        listOf(
-            Output(partOne = 4512, partTwo = UNSOLVED),
-            Output(partOne = UNSOLVED, partTwo = UNSOLVED)))
+  dayInput(
+    4,
+    listOf(
+      Output(partOne = 4512, partTwo = UNSOLVED),
+      Output(partOne = UNSOLVED, partTwo = UNSOLVED)))
